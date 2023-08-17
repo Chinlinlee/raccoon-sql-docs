e98281802aac808e8f0e58995c5cb53eb239f8ac
CHIN\a5566 <a5566qq2581@gmail.com>
2023-08-17 12:42:08 +0800

    chore: add img alt in card list

--- src/components/card-list.svelte
+++ src/components/card-list.svelte
@@ -6,7 +6,7 @@
     {#each items as item}
         <div class="my-2 rounded shadow-lg shadow-gray-200 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-300 hover:-translate-y-1">
             <figure>
-                <img src="{item.src}" class="rounded-t h-72 w-full object-cover">
+                <img src="{item.src}" alt="{item.title}" class="rounded-t h-72 w-full object-cover">
                 <figcaption>
                     <p
                         class="text-center text-lg mb-4 font-bold leading-relaxed text-gray-800 dark:text-gray-300"
